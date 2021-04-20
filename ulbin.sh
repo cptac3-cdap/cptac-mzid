04b3b69623828e516a8bf5f4d5a136b1b9120a5f
Nathan Edwards <edwardsnj@gmail.com>
2021-04-20 18:56:03 -0400

    make build work properly

--- ulbin.sh
+++ ulbin.sh
@@ -4,6 +4,6 @@ OS=`uname`
 AR=`uname -m`
 XX="linux-$AR"
 mkdir -p build dist
-./tobin.py build/cptac-mzid-${VER}.${XX} psmextract.py checkMzMLsha1.py msconvertall.py packageraw.py
-mv build/cptac-mzid-${VER}.${XX}.tgz dist
+./tobin.py build/cptacmzid-${VER}.${XX} psmextract.py checkMzMLsha1.py msconvertall.py packageraw.py
+mv build/cptacmzid-${VER}.${XX}.tgz dist
 
